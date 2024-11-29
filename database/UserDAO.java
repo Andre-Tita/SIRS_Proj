1a2b2f117190bb481437386b76c50618cc3c0a8d
andre217 <andre217@kali.tecnico.ulisboa.pt>
2024-11-29 16:30:01 +0000

    Initial commit, dirs created, non-functional.

---
+++ database/UserDAO.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+    private Connection c;
+
+    public UserDAO(Connection c) {
+        this.c = c;
+    }
+
+    public void addUser(String username, String password, String publicKey) throws SQLException {
+        String query = "INSERT INTO Users (username, password, public_key) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = c.preparedStatement(query)) {
+            statement.setStirng();
+        }
+    }
+
+    public 
+}
